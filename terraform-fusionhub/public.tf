4ad4273b9e888740a9778d000784c6f9d111818a
Jeremiah Gearheart <jeremiah.gearheart@ge.com>
2017-05-23 22:56:40 -0400

    added 10/8 route

--- terraform-fusionhub/public.tf
+++ terraform-fusionhub/public.tf
@@ -123,7 +123,7 @@ provisioner "file" {
          "sudo aws ec2 authorize-security-group-ingress --group-id ${aws_security_group.cosmos-vrouter_region1.id} --protocol tcp --port 22 --cidr 203.0.113.0/24 --region us-west-2",
          "sudo docker login -e jeremiah.gearheart@pearson.com -u _json_key -p \"$(cat gcrtest.json)\" https://gcr.io",
          "sudo docker pull gcr.io/pearson-techops/cosmos/vrouter:COS-29",
-         "sudo docker run --net host --privileged -e USERNAME=${var.USERNAME} -e PASSWORD=${var.PASSWORD} -e ETCD_DISCOVER=discover.blue-etcd.shared.prsn-dev.io -e NAMESPACE=test5 -e CORE=yes --name vrouter -itd gcr.io/pearson-techops/cosmos/vrouter:COS-29",
+         "sudo docker run --net host --privileged -e USERNAME=${var.USERNAME} -e PASSWORD=${var.PASSWORD} -e ETCD_DISCOVER=discover.blue-etcd.shared.prsn-dev.io -e NAMESPACE=test6 -e CORE=yes --name vrouter -itd gcr.io/pearson-techops/cosmos/vrouter:COS-29",
          "HOSTNAME=$(hostname | tr - _)"
          #"curl -X PUT https://root:${var.PASSWORD}@blue-etcd.shared.prsn-dev.io.:443/v2/keys/$HOSTNAME/routetableid -d value=\"${aws_route_table.us-west-2a-public.id}\"",
          #"curl -X PUT https://root:${var.PASSWORD}@blue-etcd.shared.prsn-dev.io.:443/v2/keys/$HOSTNAME/cidrblock -d value=\"${aws_subnet.us-west-2a-public.cidr_block}\"",
@@ -260,7 +260,7 @@ provisioner "file" {
            "sudo aws ec2 authorize-security-group-ingress --group-id ${aws_security_group.cosmos_vrouter_region2.id} --protocol tcp --port 22 --cidr 203.0.113.0/24 --region us-east-2",
            "sudo docker login -e jeremiah.gearheart@pearson.com -u _json_key -p \"$(cat gcrtest.json)\" https://gcr.io",
            "sudo docker pull gcr.io/pearson-techops/cosmos/vrouter:COS-29",
-           "sudo docker run --net host --privileged -e USERNAME=${var.USERNAME} -e PASSWORD=${var.PASSWORD} -e ETCD_DISCOVER=discover.blue-etcd.shared.prsn-dev.io -e NAMESPACE=test5 -e CORE=yes --name vrouter -itd gcr.io/pearson-techops/cosmos/vrouter:COS-29",
+           "sudo docker run --net host --privileged -e USERNAME=${var.USERNAME} -e PASSWORD=${var.PASSWORD} -e ETCD_DISCOVER=discover.blue-etcd.shared.prsn-dev.io -e NAMESPACE=test6 -e CORE=yes --name vrouter -itd gcr.io/pearson-techops/cosmos/vrouter:COS-29",
            "HOSTNAME=$(hostname | tr - _)"
           #"curl -X PUT https://root:${var.PASSWORD}@blue-etcd.shared.prsn-dev.io.:443/v2/keys/$HOSTNAME/routetableid -d value=\"${aws_route_table.us-east-2a-public.id}\"",
           #"curl -X PUT http://ec2-52-9-39-36.us-west-1.compute.amazonaws.com:2379/v2/keys/$HOSTNAME/cidrblock -d value=\"${aws_subnet.us-east-2a-public.cidr_block}\"",
@@ -299,12 +299,12 @@ provisioner "file" {
         }
       }
 
-/*
+
 
 resource "aws_route" "route" {
   provider = "aws.oregon"
   route_table_id = "${aws_route_table_association.us-west-2a-public.route_table_id}"
-  destination_cidr_block = "${aws_vpc.cosmos-vrouter-corerouter-region2-vpc.cidr_block}"
+  destination_cidr_block = "10.0.0.0/8"
   instance_id = "${aws_instance.cosmos-vrouter.id}"
 }
 
@@ -312,10 +312,10 @@ resource "aws_route" "route" {
 resource "aws_route" "route2" {
   provider = "aws.ohio"
   route_table_id = "${aws_route_table_association.us-east-2a-public.route_table_id}"
-  destination_cidr_block = "${aws_vpc.cosmos-vrouter-corerouter-region1-vpc.cidr_block}"
+  destination_cidr_block = "10.0.0.0/8"
   instance_id = "${aws_instance.cosmos-vrouter-region2.id}"
 }
-*/
+
 #output "Region1 Vouter Public IP:" {
 #  value = "${aws_instance.cosmos-vrouter-region1.public_ip}"
 #}
