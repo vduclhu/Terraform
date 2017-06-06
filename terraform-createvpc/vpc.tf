d2375628b80c150c83ea6ca16c62906348353d3c
Jeremiah Gearheart <jeremiah.gearheart@ge.com>
2017-06-06 11:58:56 -0400

    created base vpc plan

---
+++ terraform-createvpc/vpc.tf
@@ -0,0 +1,41 @@
+resource "aws_vpc" "cosmos-vpc" {   
+    enable_dns_hostnames = true
+    tags {
+        Name = "cosmos-vpc"
+
+    }
+}
+
+resource "aws_internet_gateway" "cosmos-vpc" {    
+    vpc_id = "${aws_vpc.cosmos-vpc.id}"
+}
+
+resource "aws_subnet" "public" {
+    vpc_id = "${aws_vpc.cosmos--vpc.id}"
+
+    cidr_block = "10.0.0.0/16"
+    availability_zone = "${var.aws_availability_zones[var.aws_region]}"
+
+    tags {
+        Name = "Public Subnet"
+    }
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = "${aws_vpc.cosmos-vpc.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.cosmos-vpc.id}"
+    }
+    tags {
+        Name = "Public Subnet"
+        environment = "cosmos-test"
+    }
+}
+
+resource "aws_route_table_association" "public" {
+    subnet_id = "${aws_subnet.public.id}"
+    route_table_id = "${aws_route_table.public.id}"
+}
+
