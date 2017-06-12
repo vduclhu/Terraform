43820f26bd1a07cf0b441a8b694ae9f759104976
Jeremiah Gearheart <jeremiah.gearheart@ge.com>
2017-06-12 10:36:24 -0400

    added eth1 back in

--- terraform-createvpc/public.tf
+++ terraform-createvpc/public.tf
@@ -67,6 +67,12 @@ resource "aws_security_group" "cosmos-vrouter_region1" {
         environment = "cosmos-test"
     }
 }
+
+resource "aws_network_interface" "vrouter" {
+  subnet_id                   = "${aws_subnet.public2.id}"
+  security_groups             = ["${aws_security_group.cosmos-vrouter_region1.id}"]
+}
+
 resource "aws_key_pair" "cosmos-admin" {
   key_name = "cosmos-admin3"
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
