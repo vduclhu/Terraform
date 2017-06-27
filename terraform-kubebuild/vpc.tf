8d8590bb0448c348351ace0832144b8eb1a34efc
Jeremiah Gearheart <jeremiah.gearheart@ge.com>
2017-06-27 17:04:33 -0400

    added bracket

--- terraform-kubebuild/vpc.tf
+++ terraform-kubebuild/vpc.tf
@@ -61,18 +61,18 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public1" {
-    subnet_id = "aws_subnet" "kubminion_subnet1"
+    subnet_id = "${aws_subnet.kubminion_subnet1.id}"
     route_table_id = "${aws_route_table.public.id}"
 }
 resource "aws_route_table_association" "public2" {
-    subnet_id = "aws_subnet" "kubminion_subnet2"
+    subnet_id = "${aws_subnet.kubminion_subnet2.id}"
     route_table_id = "${aws_route_table.public.id}"
 }
 resource "aws_route_table_association" "public3" {
-    subnet_id = "aws_subnet" "kubminion_subnet3"
+    subnet_id = "${aws_subnet.kubminion_subnet3.id}"
     route_table_id = "${aws_route_table.public.id}"
 }
 resource "aws_route_table_association" "public4" {
-    subnet_id = "aws_subnet" "kubminion_subnet4"
+    subnet_id = "${aws_subnet.kubminion_subnet4.id}"
     route_table_id = "${aws_route_table.public.id}"
 }
