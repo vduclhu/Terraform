2442a02e595a0f78868fd2a0d875356356645824
Jeremiah Gearheart <jeremiah.gearheart@ge.com>
2017-06-28 11:36:37 -0400

    adf

--- terraform-kubebuild/vpc.tf
+++ terraform-kubebuild/vpc.tf
@@ -63,11 +63,7 @@ resource "aws_route_table" "public" {
 }
 resource "aws_route_table" "private" {
     vpc_id = "${aws_vpc.kubelab_vpc.id}"
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.kubelab_vpc.id}"
-    }
+}
     tags {
         Name = "Private route table"
         environment = "kubelab_test_private"
