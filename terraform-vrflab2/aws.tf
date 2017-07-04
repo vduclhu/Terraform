37d28414dfbfecdf0e70389ac63abce2e93ea69d
root <root@ubuntu-xenial.localdomain>
2017-07-04 16:45:05 +0000

    fixed vpc module call

--- terraform-vrflab2/aws.tf
+++ terraform-vrflab2/aws.tf
@@ -1,13 +1,5 @@
 provider "aws" {
-  alias = "oregon"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region = "${var.aws_region1}"
 }
 
-provider "aws" {
-  alias = "ohio"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region = "${var.aws_region2}"
-}
--- terraform-vrflab2/main.tf
+++ terraform-vrflab2/main.tf
@@ -4,31 +4,12 @@ module "vpc" {
   name = "cosmos-vrftest-tf"
 
   cidr = "10.101.0.0/16"
-  public_subnets  = ["10.101.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.101.101.0/24", "10.101.102.0/24", "10.101.103.0/24"]
 
   azs      = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
   tags {
     "Terraform" = "true"
-    "Environment" = "${var.environment}"
+    "Environment" = "cosmos-test-vrfs"
   }
-}
-variable "cidr" {
-  description = "The CIDR block for the VPC"
-  default     = "10.101.0.0/16"
-}
-
-variable "instance_tenancy" {
-  description = "A tenancy option for instances launched into the VPC"
-  default     = "default"
-}
-
-variable "public_subnets" {
-  description = "A list of public subnets inside the VPC."
-  default     = ["10.101.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
-
-variable "azs" {
-  description = "A list of Availability zones in the region"
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
 }
