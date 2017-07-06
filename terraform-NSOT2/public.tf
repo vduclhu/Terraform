b29957889666f18402493a259fec45b5f556c397
Jeremiah Gearheart <jeremiah.gearheart@ge.com>
2017-07-06 13:58:47 -0400

    typo

--- terraform-NSOT2/public.tf
+++ terraform-NSOT2/public.tf
@@ -95,7 +95,7 @@ resource "aws_instance" "cosmos-NSOT" {
            "echo Y | sudo pip install pynsot",
            "sudo nsot-server init",
            "chmod +x /tmp/generate-nsot-configs.sh",
-           "echo sudo /tmp/generate-nsot-configs.sh ${var.RDS_NAME} ${var.RDS_USER} ${var.RDS_PASS} ${var.RDS_HOST} ${var.RDS_PORT}",
+           "sudo /tmp/generate-nsot-configs.sh ${var.RDS_NAME} ${var.RDS_USER} ${var.RDS_PASS} ${var.RDS_HOST} ${var.RDS_PORT}",
            "cat input.txt | sudo nsot-server start &"
       ]
   }
