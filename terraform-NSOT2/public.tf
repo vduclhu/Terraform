73fe3f929cbdcc73cb3328581651fa73ef07b43c
Jeremiah Gearheart <jeremiah.gearheart@ge.com>
2017-07-06 15:09:12 -0400

    trying setsid to start nsot-server

--- terraform-NSOT2/public.tf
+++ terraform-NSOT2/public.tf
@@ -96,7 +96,7 @@ resource "aws_instance" "cosmos-NSOT" {
            "sudo nsot-server init",
            "chmod +x /tmp/generate-nsot-configs.sh",
            "sudo /tmp/generate-nsot-configs.sh ${var.RDS_NAME} ${var.RDS_USER} ${var.RDS_PASS} ${var.RDS_HOST} ${var.RDS_PORT}",
-           "cat input.txt | sudo nohup nsot-server start"
+           "cat input.txt | sudo setsid nsot-server start &"
       ]
   }
   connection {
