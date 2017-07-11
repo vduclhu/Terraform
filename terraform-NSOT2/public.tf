4399f0807a95083f2e0d1b8238a05897412a34be
Jeremiah Gearheart <jeremiah.gearheart@ge.com>
2017-07-11 15:59:37 -0400

    fixed typo

--- terraform-NSOT2/public.tf
+++ terraform-NSOT2/public.tf
@@ -189,12 +189,12 @@ resource "aws_elb" "cosmos-NSOT" {
 }
 
 output "NSOT ELB DNS: " {
-    value = {aws_elb.cosmos-NSOT.dns_name}
+    value = ${aws_elb.cosmos-NSOT.dns_name}
 }
 output "NSOT App Server1 - ${aws_instance.cosmos-NSOT1.availability_zone} Public IP: " {
-    value = {aws_instance.cosmos-NSOT1.public_ip}
+    value = ${aws_instance.cosmos-NSOT1.public_ip}
 }
 output "NSOT App Server2 - ${aws_instance.cosmos-NSOT2.availability_zone} Public IP: " {
-    value = {aws_instance.cosmos-NSOT2.public_ip}
+    value = ${aws_instance.cosmos-NSOT2.public_ip}
 }
  
