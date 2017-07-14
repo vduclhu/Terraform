17859ff3d1b53499ed1639ab287473a2315120a8
Jeremiah Gearheart <jeremiah.gearheart@ge.com>
2017-07-13 23:24:38 -0400

    afdsf

--- terraform-fusionhub-csr/public.tf
+++ terraform-fusionhub-csr/public.tf
@@ -196,12 +196,11 @@ provisioner "file" {
     provisioner "remote-exec" {
         inline = [
            "conf t",
-           "copy tftp://${aws_instance.cosmos-testbox-region1.private_ip}/csr-configs/test startup-config",
+           "copy tftp://${aws_instance.cosmos-testbox-region1.private_ip}/csr-configs/test running-config",
            "",
            "",
            "",
            "",
-           "reload",
            ""
       ]
   }
