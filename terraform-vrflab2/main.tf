983262a8a49da059000d17d1b0398cc22e056a55
root <root@ubuntu-xenial.localdomain>
2017-07-26 22:47:57 +0000

    asfsafdd

--- terraform-vrflab2/main.tf
+++ terraform-vrflab2/main.tf
@@ -32,38 +32,3 @@ module "vrouter" {
   }
  
 }
-
-module "vpc2" {
-  source = "./modules/vpc"
-  provider = "aws.cali"
-  name = "cosmos-vrftest-tf"
-  cidr = "10.102.0.0/16"
-  public_subnet  = "10.102.0.0/24"
-  region = "us-west-1"
-  azone      = "us-west-1b"
-
-  tags {
-    "Terraform" = "true"
-    "Environment" = "cosmos-test-vrfs"
-  }
-}
-module "vrouter2" {
-  source = "./modules/vrouter"
-  provider = "aws"
-  vpc_id =  "${module.vpc2.vpc_id}"
-  name = "cosmos-vrftest-tf"
-  vrouter_instance_type = "t2.small"
-  public_subnet  = "${module.vpc2.public_subnet}"
-  region = "us-west-1"
-  azone      = "us-west-1b"
-  USERNAME="root"
-  PASSWORD="WKq3d"
-  NAME_SPACE="pog11"
-  CORE="yes"
-
-  tags {
-    "Terraform" = "true"
-    "Environment" = "cosmos-test-vrfs"
-  }
- 
-}
