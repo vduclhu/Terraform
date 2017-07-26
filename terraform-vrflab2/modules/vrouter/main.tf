c10d808dfbe90aea004b22917fdd658fdc467da8
Jeremiah Gearheart <jeremiah.gearheart@ge.com>
2017-07-26 11:36:57 -0400

    changed to green cluster

--- terraform-vrflab2/modules/vrouter/main.tf
+++ terraform-vrflab2/modules/vrouter/main.tf
@@ -87,7 +87,7 @@ provisioner "file" {
            "echo Y | sudo apt-get install jq",
          "sudo docker login -e jeremiah.gearheart@pearson.com -u _json_key -p \"$(cat gcrtest.json)\" https://gcr.io",
          "sudo docker pull gcr.io/pearson-techops/cosmos/vrouter:COS-29",
-         "sudo docker run --net host --privileged -e USERNAME=${var.USERNAME} -e PASSWORD=${var.PASSWORD} -e ETCD_DISCOVER=discover.blue-etcd.shared.prsn-dev.io -e NAMESPACE=${var.NAME_SPACE} -e CORE=${var.CORE} --name vrouter -itd gcr.io/pearson-techops/cosmos/vrouter:COS-29",
+         "sudo docker run --net host --privileged -e USERNAME=${var.USERNAME} -e PASSWORD=${var.PASSWORD} -e ETCD_DISCOVER=discover.green-etcd.shared.prsn-dev.io -e NAMESPACE=${var.NAME_SPACE} -e CORE=${var.CORE} --name vrouter -itd gcr.io/pearson-techops/cosmos/vrouter:COS-29",
          "HOSTNAME=$(hostname | tr - _)"
         
       ]
