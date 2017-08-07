70c2cb1bb265c07ec75b51b7e323420da8820caf
root <root@ubuntu-xenial.localdomain>
2017-08-07 20:19:45 +0000

    added testbox to edge

--- terraform-vrflabedge/modules/vrouter/main.tf
+++ terraform-vrflabedge/modules/vrouter/main.tf
@@ -95,4 +95,16 @@ resource "aws_instance" "cosmos-vrouter" {
     }
 
   }
- 
+resource "aws_instance" "cosmos-testbox" {
+    ami = "${var.ami_region}"
+    availability_zone = "${var.azone}"
+    instance_type = "${var.vrouter_instance_type}"
+    key_name = "${aws_key_pair.cosmos-admin.key_name}"
+    vpc_security_group_ids = ["${aws_security_group.cosmos-vrouter_region.id}"]
+    subnet_id = "${var.public_subnet}"
+    associate_public_ip_address = true
+    source_dest_check = false
+    tags {
+        Name = "cosmos-vrouter-TF"
+    }
+} 
